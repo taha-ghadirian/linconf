0f483dce9aecb1f200dbfc044907387a1042bf1b
taha ghadirian <taha.ghadirian@outlook.com>
2019-11-27 18:41:28 +0330

    Adding ([Y],n) Question for Tor install

--- portscanner.sh
+++ portscanner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing important apps"
-sleep 2
+sleep 5
 sudo apt-get update
 sudo apt-get install snapd vlc tmux neofetch git zsh
 sleep 2
@@ -12,8 +12,14 @@ sudo snap install chromium
 sleep 2
 clear
 
-echo "installing tor-browser..."
+read -p "Do you need to install TorBrowser? ([Y],n): " yn
+if [ $yn == "y" ] || [ $yn == "Y" ] || [ $yn == "yes" ] || [ $yn == "Yes" ]
+then
+echo "Installing Tor..."
 sudo snap install tor
+else
+echo "Tor Install Aborted!"
+fi
 sleep 2
 clear
 
