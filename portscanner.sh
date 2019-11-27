fa1b64f71e4c131fcdf74986f19bd246d84cdc2e
taha ghadirian <taha.ghadirian@outlook.com>
2019-11-27 16:01:51 +0330

    Installing OhMyZsh

--- portscanner.sh
+++ portscanner.sh
@@ -4,13 +4,24 @@ echo "Installing important apps"
 sleep 2
 sudo apt-get update
 sudo apt-get install snapd vlc tmux neofetch git zsh
-
 sleep 2
-echo "installing chromium.."
+clear
+
+echo "installing chromium..."
 sudo snap install chromium
 sleep 2
 clear
 
-echo "installing tor-browser.."
-sleep 2
+echo "installing tor-browser..."
 sudo snap install tor
+sleep 2
+clear
+
+echo "Installing ohMyZsh..."
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 2
+clear
+
+echo "Done"
+sleep 10
+exit
