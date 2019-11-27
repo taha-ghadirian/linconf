9963201e6e01c3b08e64148e6347acf65511d4f5
taha ghadirian <taha.ghadirian@outlook.com>
2019-11-27 15:49:18 +0330

    Merge Taha&Pouya

--- portscanner.sh
+++ portscanner.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 echo "Installing important apps"
 sleep 2
 sudo apt-get update
 sudo apt-get install snapd vlc tmux neofetch git zsh
+
+sleep 2
+echo "installing chromium.."
+sudo snap install chromium
+sleep 2
+clear
+
+echo "installing tor-browser.."
+sleep 2
+sudo snap install tor
